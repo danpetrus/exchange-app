fcedcbc2b054e244e372bd6a20b08a90bad49310
Dan-Mihai Petrus <34897927+danpetrus@users.noreply.github.com>
2021-03-23 19:36:02 +0200

    Add files via upload

---
+++ CurrencyList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.Models
+{
+    public class CurrencyList
+    {
+        public Dictionary<string, decimal> currency_key { get; set; } = new Dictionary<string, decimal>();
+    }
+}
---
+++ Get_Currencies.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace ArrPairs
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetStringAsync("https://v6.exchangerate-api.com/v6/7438098b253ff4a245c445c0/latest/RON");
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+
+            JObject items = JObject.Parse(result);
+
+            var id = items["conversion_rates"].ToString();
+
+            Console.WriteLine(id);
+
+        }
+    }
+
+}
+
